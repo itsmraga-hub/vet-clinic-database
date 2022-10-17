d53b4968e8d227315ce825a7646a223675c49863
Raga <ragawilliam570@gmail.com>
2022-10-17 17:01:57 +0300

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+-- CREATE TABLE animals (
+--     name varchar(100)
+-- );
+
+CREATE TABLE animals(
+    id INT primary key not null,
+    name TEXT not null,
+    date_of_birth DATE,
+    escape_attempts INT not null,
+    neutered BOOL,
+    weight_kg DECIMAL
 );
