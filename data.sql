e04604563875e754485089e370cfc56c3006aea0
Raga <ragawilliam570@gmail.com>
2022-10-18 14:10:10 +0300

    Insert correct data

--- data.sql
+++ data.sql
@@ -12,13 +12,13 @@ INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_
 
 INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
 
-INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020-02-08', 0, false, 11);
+INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020-02-08', 0, false, -11);
 
-INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', '2021-11-15', 2, true, 5.7);
+INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7);
 
-INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', '1993-04-02', 3, false, 12.13);
+INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13);
 
-INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', '2005-06-12', 1, true, 45);
+INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', '2005-06-12', 1, true, -45);
 
 INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
 
