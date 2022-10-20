25ed55193d200d2495a31ecc89112f0d5868fb6a
Raga <ragawilliam570@gmail.com>
2022-10-20 18:15:55 +0300

    Add vets/visits and specializations tables

--- schema.sql
+++ schema.sql
@@ -42,3 +42,29 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species;
 
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners;
+
+
+-- Create a table named vets with the following columns:
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150),
+    age INT,
+    date_of_graduation DATE
+);
+
+/* There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship. */
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species (id),
+    vets_id INT REFERENCES vets (id)
+);
+
+/* There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit. */
+
+CREATE TABLE visits (
+    animals_id INT REFERENCES animals (id),
+    vets_id INT REFERENCES vets (id),
+    date_of_visit DATE
+);
+
