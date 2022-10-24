e47953395c0ef03d336ae6166b4e8870a53b4870
David-Kasilia <davidkasilia001@gmail.com>
2022-10-24 16:12:22 +0300

    Add Exceution time

--- queries.sql
+++ queries.sql
@@ -187,8 +187,8 @@ SELECT * FROM visits where vet_id = 2;
 SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 -- Query with Performance and Normalization using EXPLAIN ANALYZE
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4; -- Excecution time: 4894.559 ms
 
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2; -- Exceution Time 1367.615ms
 
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com'; -- Execution Time 879.584ms
